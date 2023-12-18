67c8129056fdeaf371b8e666cbf3d09ceb75c562
kamonphan phatkaew <154107527+kamonnn23@users.noreply.github.com>
2023-12-18 11:07:24 +0700

    เพิ่มไฟล์ week1

---
+++ week1/index.php
@@ -0,0 +1,15 @@
+<?php
+     $address = "moo.3 wattananuwong";
+     $char1 = 'A';
+     echo $address[0].$address[1].$address[2].$address[3].$address[4];
+     echo "<br>";
+     echo $char1;
+     $age = 17;
+     $pi = 3.14;
+     $pi = 3.14144524;
+
+     echo $age+$pi;
+     $istrue = false;
+     echo $istrue;
+
+?>
